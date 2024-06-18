86fb11b6732f99aab0aa9f006fea91d55fbeb0c2
Parisa <pmoshfegh@ownersbox.com>
2024-06-18 11:06:31 -0700

    add auth, signup and login

--- models/User.js
+++ models/User.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   dob: { type: Date, required: true },
   distance: { type: String, required: true },
   timeGoal: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
 
-module.exports = User;
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,26 +1,62 @@
 const express = require('express');
 const User = require('../models/User');
-const responses = require('../response');
+// const responses = require('../response');
+// const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jwtSecret = 'your_jwt_secret_key';
+
 const router = express.Router();
 
 // Create a new user
-router.post('/users', async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    const distance = user.distance;
-    console.log(responses[distance])
+// router.post('/users', async (req, res) => {
+//   try {
+//     const user = await User.create(req.body);
+//     const distance = user.distance;
+//     console.log(responses[distance])
 
-    // if (responses[distance]) {
-      console.log('here')
-      res.status(201).json({
-        user,
-        message: responses[distance].message,
-        tips: responses[distance].tips,
-        trainingPlan: responses[distance].trainingPlan,
-      });
-    // } else {
-    //   res.status(201).json({ user, message: 'Distance not recognized' });
-    // }
+//     // if (responses[distance]) {
+//       console.log('here')
+//       res.status(201).json({
+//         user,
+//         message: responses[distance].message,
+//         tips: responses[distance].tips,
+//         trainingPlan: responses[distance].trainingPlan,
+//       });
+//     // } else {
+//     //   res.status(201).json({ user, message: 'Distance not recognized' });
+//     // }
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
+
+// Create a new user
+router.post('/signup', async (req, res) => {
+  try {
+    const { name, dob, distance, timeGoal, email, password } = req.body;
+    const user = new User({ name, dob, distance, timeGoal, email, password });
+    await user.save();
+    const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
+    res.status(201).json({ token, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Login a user
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) {
+      return res.status(400).json({ error: 'Invalid email or password' });
+    }
+    const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
+    res.status(200).json({ token, user });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
