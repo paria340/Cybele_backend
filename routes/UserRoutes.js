f4044bd7e55a214b9b60687aa64122f3d6cef67c
Parisa <pmoshfegh@ownersbox.com>
2024-11-05 11:18:39 -0800

    minor backend cleanups to run project again

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const User = require('../models/User');
-// const responses = require('../response');
+const responses = require('../response');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const jwtSecret = 'your_jwt_secret_key';
 
 const router = express.Router();
-
-// Create a new user
-// router.post('/users', async (req, res) => {
-//   try {
-//     const user = await User.create(req.body);
-//     const distance = user.distance;
-//     console.log(responses[distance])
-
-//     // if (responses[distance]) {
-//       console.log('here')
-//       res.status(201).json({
-//         user,
-//         message: responses[distance].message,
-//         tips: responses[distance].tips,
-//         trainingPlan: responses[distance].trainingPlan,
-//       });
-//     // } else {
-//     //   res.status(201).json({ user, message: 'Distance not recognized' });
-//     // }
-//   } catch (error) {
-//     res.status(400).json({ error: error.message });
-//   }
-// });
 
 // Create a new user
 router.post('/signup', async (req, res) => {
